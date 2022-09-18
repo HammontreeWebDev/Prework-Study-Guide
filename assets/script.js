93d8122b25b33a8ae51f92955f34cd610e7589c3
Kaileb Hammontree <kaileb47@outlook.com>
2022-09-18 00:26:07 -0400

    commented out part of .js

--- assets/script.js
+++ assets/script.js
@@ -15,8 +15,9 @@ if (topic === 'HTML') {
 } else {
   console.log('Please try again!');
 }
-*/
+
 var shapes = ["triangle", "square", "pentagon", "circle"];
 for(var x = 0; x < shapes.length; x++) {
  console.log(shapes[x]);
 }
+*/
